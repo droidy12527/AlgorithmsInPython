143e8e61991a7395299bb8c88b58f6d19c463169
droidy12527 <gundiaramasu@gmail.com>
2021-02-27 19:08:19 +0530

    Find the last ocurance of element

---
+++ findthelastoccurance.py
@@ -0,0 +1,25 @@
+#Function to Find the last occurance of number which is repeating in an array
+
+def firstOccurance(arr, el):
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        mid = left + right // 2
+        if arr[mid] == el:
+            if mid == len(arr) or arr[mid + 1] != el: 
+                return mid
+            left = left + 1 
+        elif arr[mid] > el:
+            right = right - 1
+        else:
+            left = left + 1
+
+
+
+arr = [2,3,4,5,5,5,6,7,10]
+element = 5
+index = firstOccurance(arr, element)
+if index == -1:
+    print('The element Does not exist in the given array')
+else:
+    print('The element exist at the index {}'.format(index))
